322b04a8a72453c02b31971f70c6ebde72aff17f
Yves G <contact@webnumerik.fr>
2022-02-18 15:29:58 +0100

    feat: portfolio card

--- components/PortofolioCard.js
+++ components/PortofolioCard.js
@@ -1,11 +1,31 @@
 import React from "react";
+import styles from "../styles/Portofolio.module.css";
 
 function PortofolioCard() {
   return (
-    <>
-      <h1>Faire Passer une librairie Jquery vers React</h1>
-      <h2>Compétences déployées</h2>
-    </>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.left}><img src="http://via.placeholder.com/250x300
+" alt="" /></div>
+        <div className={styles.right}>
+          <h1>Faire Passer une librairie Jquery vers React</h1>
+          <p>
+            Convertir une application entière et ses plugins de Jquery vers
+            React en ajoutant un système de gestion d'état Redux. Déployer un
+            composant en librairie sur NPM.
+          </p>
+          <h2>Compétences déployées</h2>
+          <ul>
+            <li>- Refondre une application pour réduire la dette technique</li>
+            <li>- Analyser la performance d'une application web</li>
+            <li>- Déployer une application front-end</li>
+            <li>
+              - Programmer en JavaScript avec la programmation fonctionnelle
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- utils/gsapAnimations/portofolioGsapAnimations.js
+++ utils/gsapAnimations/portofolioGsapAnimations.js
@@ -3,19 +3,6 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// let panels = gsap.utils.toArray(".panel")
-
-// panels.forEach((panel, i) => {
-
-//   ScrollTrigger.create({
-//     trigger: panel,
-//     start: "top top",
-//     pin: i === panels.length -1 ? false : true,
-//     //end:"bottom 100",
-//     pinSpacing: false
-//   });
-// });
-
 function portofolioGsapAnimations() {
   const panel = (panelRef) => {
     console.log(panelRef);
