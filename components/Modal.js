5de07966dce09df045ba19d7ac6463549f725059
Yves G <contact@webnumerik.fr>
2022-06-25 15:29:26 +0200

    fix:

--- components/Modal.js
+++ components/Modal.js
@@ -19,12 +19,11 @@ const Modal = (props) => {
 
   if (!props.show) {
     document.body.style.overflow = "scroll";
-    
     return null;
   }
   return ReactDOM.createPortal(
     <div onClick={(e) => e.stopPropagation()}>
-      {(document.body.style.overflow = "hidden")}
+    { document.body.style.overflow = "hidden"}
       <div className={styles.modal} onClick={props.onClose}>
         <div
           className={styles.modalContent}
@@ -36,6 +35,7 @@ const Modal = (props) => {
           <div className={styles.modalHeader}>
             <Image
               className={styles.closeModal}
+              // src={closeModal}
               src="/close_modal.png"
               alt={styles.closeModal}
               onClick={props.onClose}
