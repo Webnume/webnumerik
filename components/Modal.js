43478f53fc1d7bcfc8a1a83599ede647bc265c0a
Yves G <contact@webnumerik.fr>
2022-06-25 15:52:49 +0200

    refactor:

--- components/Modal.js
+++ components/Modal.js
@@ -1,30 +1,21 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import styles from "../styles/Modal.module.css";
 import Image from "next/image";
 
 const Modal = (props) => {
+  const [mount, setMount] = useState(false);
   const closeOnEscapeKeyDown = (e) => {
     if ((e.charCode || e.keyCode) === 27) {
       props.onClose();
     }
   };
 
-  const [mount, setMount] = useState(false);
-
-  const onScroll = () => {
-    document.body.style.overflow = "scroll";
-    return null;
-  };
-
   useEffect(() => {
     setMount(true);
-    // window.addEventListener("scroll", onScroll);
-
     document.body.addEventListener("keydown", closeOnEscapeKeyDown);
     return function cleanup() {
       document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
-      // window.removeEventListener("scroll", onScroll);
     };
   });
 
@@ -32,34 +23,36 @@ const Modal = (props) => {
     document.body.style.overflow = "scroll";
     return null;
   }
-  return mount ? ReactDOM.createPortal(
-    <div onClick={(e) => e.stopPropagation()}>
-      { document.body.style.overflow = "hidden"}
-      <div className={styles.modal} onClick={props.onClose}>
-        <div
-          className={styles.modalContent}
-          onClick={(e) => e.stopPropagation()}
-          style={{
-            backgroundImage: "url(" + props.backgroundImg + ")",
-          }}
-        >
-          <div className={styles.modalHeader}>
-            <Image
-              className={styles.closeModal}
-              // src={closeModal}
-              src="/close_modal.png"
-              alt={styles.closeModal}
-              onClick={props.onClose}
-              width="25%"
-              height="25%"
-            />
+  return mount
+    ? ReactDOM.createPortal(
+        <div onClick={(e) => e.stopPropagation()}>
+          {(document.body.style.overflow = "hidden")}
+          <div className={styles.modal} onClick={props.onClose}>
+            <div
+              className={styles.modalContent}
+              onClick={(e) => e.stopPropagation()}
+              style={{
+                backgroundImage: "url(" + props.backgroundImg + ")",
+              }}
+            >
+              <div className={styles.modalHeader}>
+                <Image
+                  className={styles.closeModal}
+                  // src={closeModal}
+                  src="/close_modal.png"
+                  alt={styles.closeModal}
+                  onClick={props.onClose}
+                  width="25%"
+                  height="25%"
+                />
+              </div>
+              <div className={styles.modalBody}>{props.children}</div>
+            </div>
           </div>
-          <div className={styles.modalBody}>{props.children}</div>
-        </div>
-      </div>
-    </div>,
-    document.getElementById("__next")
-  ): null;
+        </div>,
+        document.getElementById("__next")
+      )
+    : null;
 };
 
 export default Modal;
