a41e787b8f9597b3291aa18663297717be88b425
Yves G. <contact@webnumerik.fr>
2025-02-11 02:04:24 +0100

    fix:

--- pages/api/contact/route.ts
+++ pages/api/contact/route.ts
@@ -14,8 +14,8 @@ export default async function POST(req) {
         user: process.env.EMAIL_USER, // Your email
         pass: process.env.EMAIL_PASS, // Your email password or app password
       },
-      socketTimeout: 10000, // Timeout after 10 seconds
-      connectionTimeout: 10000, // Limit connection time
+      socketTimeout: 20000, // Timeout after 10 seconds
+      connectionTimeout: 20000, // Limit connection time
     });
 
     // Email content
